bcf5ba7e6bd5b1915dbf62c35c71d2826e632776
Pinkp0ny <a.kurganovv@gmail.com>
2023-01-14 21:59:21 +0300

    step 2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,5 +3,7 @@ import { program } from 'commander';
 
 program
   .version('0.1.0')
-  .description('Compares two configuration files and shows a difference.');
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format <type>', 'Output format')
+  .argument('<filepath1> <filepath2>');
 program.parse();

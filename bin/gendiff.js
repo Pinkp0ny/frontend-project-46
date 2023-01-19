509a10450bdb642e1bfb27807478f87180e2f1fe
Pinkp0ny <a.kurganovv@gmail.com>
2023-01-19 18:01:36 +0300

    step 3

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,9 +1,14 @@
 #!/usr/bin/env node
 import { program } from 'commander';
+import genDiff from '../src/index.js';
 
 program
   .version('0.1.0')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format <type>', 'Output format')
-  .argument('<filepath1> <filepath2>');
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .action((filepath1, filepath2) => {
+    console.log(genDiff(filepath1, filepath2));
+  });
 program.parse();
---
+++ src/index.js
@@ -0,0 +1,34 @@
+import _ from 'lodash';
+import path from 'path';
+import fs from 'fs';
+
+export default (filePath1, filePath2) => {
+  const parsedFile1 = JSON.parse(fs.readFileSync(path.resolve(process.cwd(), filePath1)));
+  const parsedFile2 = JSON.parse(fs.readFileSync(path.resolve(process.cwd(), filePath2)));
+  const gendiff = (object1, object2) => {
+    const keys1 = _.sortBy(Object.keys(object1));
+    const result = {};
+    for (let i = 0; i < keys1.length; i += 1) {
+      const key1 = keys1[i];
+      if (!object2[key1]) {
+        result[`-${key1}`] = object1[key1];
+      } else if (object1[key1] === object2[key1]) {
+        result[`${key1}`] = object1[key1];
+      } else {
+        result[`+${key1}`] = object1[key1];
+        result[`-${key1}`] = object2[key1];
+      }
+    }
+
+    const keys2 = _.sortBy(Object.keys(object2));
+    for (let i = 0; i < keys2.length; i += 1) {
+      const key2 = keys2[i];
+      if (!object1[key2]) {
+        result[`-${key2}`] = object2[key2];
+      }
+    }
+    return result;
+  };
+  const diff = gendiff(parsedFile1, parsedFile2);
+  return (diff);
+};

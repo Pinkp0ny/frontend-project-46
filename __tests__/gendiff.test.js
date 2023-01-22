62d21076dce0473a06320dd423eec20aa6b383f3
Pinkp0ny <a.kurganovv@gmail.com>
2023-01-22 20:33:34 +0300

    add tests

---
+++ __tests__/gendiff.test.js
@@ -0,0 +1,25 @@
+import { test, expect } from '@jest/globals';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import gendiff from '../src/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+
+const first = getFixturePath('file1.json');
+const second = getFixturePath('file2.json');
+
+const expected = {
+  '-follow': false,
+  host: 'hexlet.io',
+  '-proxy': '123.234.53.22',
+  '+timeout': 50,
+  '-timeout': 20,
+  '-verbose': true,
+};
+
+test('gendiff', () => {
+  expect(gendiff(first, second)).toEqual(expected);
+});

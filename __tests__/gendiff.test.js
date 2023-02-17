a287c04c4772c52057e32fbac41f0c8edda9de0b
Pinkp0ny <a.kurganovv@gmail.com>
2023-02-17 19:14:01 +0300

    update tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -10,12 +10,12 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
 test.each([
-  ['file1.json', 'file2.json', 'stylish', stylishResult],
-  ['file1.json', 'file2.json', 'plain', plainResult],
-  ['file1.json', 'file2.json', 'json', jsonResult],
-  ['file1.yaml', 'file2.yaml', undefined, stylishResult],
-  ['file1.yaml', 'file2.yaml', 'plain', plainResult],
-  ['file1.yaml', 'file2.yaml', 'json', jsonResult],
-])('diff', (filePath1, filePath2, format, result) => {
+  ['file1.json', 'file2.json', stylishResult, 'stylish'],
+  ['file1.json', 'file2.json', plainResult, 'plain'],
+  ['file1.json', 'file2.json', jsonResult, 'json'],
+  ['file1.yaml', 'file2.yaml', stylishResult, undefined],
+  ['file1.yaml', 'file2.yaml', plainResult, 'plain'],
+  ['file1.yaml', 'file2.yaml', jsonResult, 'json'],
+])('Compare %s and %s to expect %s in "%s" style', (filePath1, filePath2, result, format) => {
   expect(gendiff(getFixturePath(filePath1), getFixturePath(filePath2), format)).toEqual(result);
 });
